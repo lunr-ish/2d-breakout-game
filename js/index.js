aa347e7c35fdd4dff0764b30e313575639a2e954
lunr_ish <174506670+lunr-ish@users.noreply.github.com>
2024-07-22 23:35:43 +0530

    add a rectangle with a 50% blue stroke on canvas

--- js/index.js
+++ js/index.js
@@ -14,3 +14,10 @@ ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
 ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
+
+// STROKE RECT
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgb(0 0 255 / 50%)";
+ctx.stroke();
+ctx.closePath();
