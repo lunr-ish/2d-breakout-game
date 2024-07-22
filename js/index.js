bdcb539c0ff14cd65a57a3866efac65278143cb2
lunr_ish <174506670+lunr-ish@users.noreply.github.com>
2024-07-22 23:55:27 +0530

    add logic to change the color of the ball to a random color every time it hits the wall

--- js/index.js
+++ js/index.js
@@ -1,18 +1,27 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+const ballRadius = 10;
+let ballColor = "#0095DD";
+
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
 let dx = 2;
 let dy = -2;
 
-const ballRadius = 10;
+function getRandomByte() {
+  return Math.floor(Math.random() * 256);
+}
+
+function getRandomColor() {
+  return `rgb(${getRandomByte()} ${getRandomByte()} ${getRandomByte()})`;
+}
 
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = ballColor;
   ctx.fill();
   ctx.closePath();
 }
@@ -22,12 +31,14 @@ function draw() {
 
   drawBall();
 
-  if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
+    ballColor = getRandomColor();
   }
 
-  if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
+    ballColor = getRandomColor();
   }
 
   x += dx;
