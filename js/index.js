fc5fba9729e76a93fb4118d203df727ea4f3b04c
lunr_ish <174506670+lunr-ish@users.noreply.github.com>
2024-07-22 23:32:32 +0530

    add basic red square on canvas

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+
+// RED SQUARE
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
