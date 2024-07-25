74a2d80751fc154451491a292dcf19526c2e5b92
lunr_ish <174506670+lunr-ish@users.noreply.github.com>
2024-07-25 21:02:11 +0530

    add winning logic and message

--- js/index.js
+++ js/index.js
@@ -149,6 +149,12 @@ function collisionDetection() {
         dy = -dy;
         b.status = 0;
         score++;
+
+        if (score == brickRowCount * brickColumnCount) {
+          alert("YOU WIN, CONGRATULATIONS!");
+          document.location.reload();
+          clearInterval(interval);
+        }
       }
     }
   }
